118f6c17ff3065ecece6028b54ba4482b0f08847
sajidsallu <sajidsallu0000@gmail.com>
2022-10-14 10:21:02 +0530

    first commit

---
+++ src/components/SingleProduct.js
@@ -0,0 +1,69 @@
+import { Card, Button } from "react-bootstrap";
+import { CartState } from "../context/Context";
+import Rating from "./Rating";
+import LazyLoad from 'react-lazy-load';
+
+
+const SingleProduct = ({ prod }) => {
+  const {
+    state: { cart },
+    dispatch,
+  } = CartState();
+
+  return (
+    <>
+    
+    <div className="products">
+    <LazyLoad height={500}>
+    <Card>
+    
+
+        <Card.Img variant="top" src={prod.image} alt={prod.name} />
+        <Card.Body>
+          <Card.Title>{prod.name}</Card.Title>
+          <Card.Subtitle style={{ paddingBottom: 10 }}>
+            <span>₹ {prod.price.split(".")[0]}</span>
+            {prod.fastDelivery ? (
+              <div>Fast Delivery</div>
+            ) : (
+              <div>4 days delivery</div>
+            )}
+            <Rating rating={prod.ratings} />
+          </Card.Subtitle>
+          {cart.some((p) => p.id === prod.id) ? (
+            <Button
+              variant="danger"
+              onClick={() =>
+                dispatch({
+                  type: "REMOVE_FROM_CART",
+                  payload: prod,
+                })
+              }
+            >
+              Remove from Cart
+            </Button>
+          ) : (
+            <Button
+              onClick={() =>
+                dispatch({
+                  type: "ADD_TO_CART",
+                  payload: prod,
+                })
+              }
+              disabled={!prod.inStock}
+            >
+              {!prod.inStock ? "Out of Stock" : "Add to Cart"}
+            </Button>
+          )}
+        </Card.Body>
+      
+      </Card>
+      </LazyLoad>
+      
+    </div>
+    
+    </>
+  );
+};
+
+export default SingleProduct;
